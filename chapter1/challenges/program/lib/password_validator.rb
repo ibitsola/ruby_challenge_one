ad51f29547b323f90c0b20a0a689db649fada096
Ira Bitsola <ira.bitsola@yahoo.com>
2022-11-04 10:14:55 +0000

    blank validator

--- chapter1/challenges/program/lib/password_validator.rb
+++ chapter1/challenges/program/lib/password_validator.rb
@@ -1,17 +1,6 @@
 # Valid passowords are
 # - 8 or more characters
 # - include at least one of these characters: !, @, $, %, &
-# - minor change
 
 def valid?(password)
-    if password.length >= 8 && (password.include?("!") || password.include?("@") || password.include?("$") || password.include?("%") || password.include?("&"))
-      return true
-    else
-      return false
-    end
 end
-
-def 
-  
-my_pass = "Hello!"
-puts valid?(my_pass)  

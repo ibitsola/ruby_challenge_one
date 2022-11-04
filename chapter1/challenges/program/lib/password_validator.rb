231869eda256850b5351eb6ff43cf8e8fd50d604
Ira Bitsola <ira.bitsola@yahoo.com>
2022-11-04 10:31:32 +0000

    Final code on peer_review

--- chapter1/challenges/program/lib/password_validator.rb
+++ chapter1/challenges/program/lib/password_validator.rb
@@ -3,4 +3,9 @@
 # - include at least one of these characters: !, @, $, %, &
 
 def valid?(password)
+  if password.length >= 8 && (password.include?("!") || password.include?("@") || password.include?("$") || password.include?("%") || password.include?("&"))
+    return true
+  else
+    return false
+  end
 end
